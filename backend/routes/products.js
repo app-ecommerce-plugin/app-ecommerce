414e0a285b025c344b8a36d972409021d7030d21
José Manuel <josemanuel.pina@rtve.es>
2025-05-28 16:22:42 +0200

    Solución redisClient

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const redisClient = require('../redis');
+const redisClient = require('../utils/redisClient');
 
 // Ruta para guardar productos seleccionados
 router.post('/selected', async (req, res) => {
